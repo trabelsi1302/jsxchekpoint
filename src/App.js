06d077477be7a6a31e7bf43fc1d97b17fdcf1910
trabelsi1302 <elyestrfr@yahoo.fr>
2023-03-22 15:13:29 +0100

    elyes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import Address from "./component/Profils/Address";
 import FullName from "./component/Profils/FullName";
 import ProfilePhoto from "./component/Profils/ProfilePhoto";
-
+import Email from "./component/Profils/Email";
 function App() {
+  const name='elytes trabelsi'
+  const style={color:"red"}
+
   return (
     <div className="App">
       <header className="App-header">   
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{name}</h1>
    <ProfilePhoto />
    <Address />
    <FullName />
+   <Email name={name} style={style}/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/component/Profils/Email.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Email = ({name , style}) => {
+  return (
+    <div style={style}>
+      <p>Elyes</p>
+      <h1>elytes,{name}</h1>
+
+    </div>
+  )
+}
+
+export default Email
